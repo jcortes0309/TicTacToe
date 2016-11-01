7d750a3e441af61952dc1413afb9b43f34c0826d
Juan A Cortes <juan.cortes@gmail.com>
2016-11-01 16:32:01 -0400

    Game winner: If "o" or "x" make a row, declare the winner by putting alert text on an element on the page. 	1. Select the button elements using a CSS selector 	2. Get the text values of the individual buttons into an array (you have the option of doing either 1-dimensional or 2-dimensional array) 	3. Write a function or use one you've written to determine the winner 	4. Announce the winner

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,32 +1,19 @@
 $(document).ready(function () {
   var counter = 0;
   var square_value = "";
-  // $(".square").on("click", function () {
-  //   // debugger
-  //   // if (square_value === 'O') {
-  //   //
-  //   // } else {
-      // if (counter === 0) {
-      //   $(this).text("O");
-      //   counter = 1;
-      //   console.log("counter var = " + counter);
-      // } else if (counter === 1) {
-      //   $(this).text("X");
-      //   counter = 2;
-      //   console.log("counter var in else if = " + counter);
-      // } else {
-      //   $(this).text("");
-      //   counter = 0;
-      //   console.log(("counter var in else = " + counter));
-      // }
-  //     square_value = $(this).text();
-  //     console.log("Square value is " + square_value);
-  //   // }
-  // });
+  var board = ["","","","","","","","",""];
+
+  $(".play_again").on("click", function () {
+    $(this).hide();
+    board = ["","","","","","","","",""];
+    $(".square")
+      .prop("disabled", false)
+      .text("");
+    $(".winner").hide();
+  });
 
   $(".square").on("click", function () {
     square_value = $(this).text();
-    console.log("Square value is " + square_value);
     if (square_value === "") {
       if (counter === 0) {
         $(this).text("O");
@@ -35,8 +22,67 @@ $(document).ready(function () {
       } else if (counter === 1) {
         $(this).text("X");
         counter = 0;
-        console.log("counter var in else if = " + counter);
+      }
+      var button_name = $(this).attr('name');
+      var button_value = $(this).text();
+      push_to_board(button_name, button_value);
+      var winner = tictactoe(board);
+      if (winner) {
+        $("p")
+          .text(winner + " WINS!")
+          .show();
+        $(".square").prop("disabled", true);
+        $(".play_again").show();
+        console.log(board);
+      } else {
+        console.log("Keep playing");
+        console.log(board.length);
       }
     }
   });
+
+  function push_to_board(name, value) {
+    var location = Number(name);
+    board[location] = value;
+  }
+
+  function isWinner(board, player) {
+    if (board[0] === player && board[1] === player && board[2] === player) {
+      return true;
+    }
+    if (board[3] === player && board[4] === player && board[5] === player) {
+      return true;
+    }
+    if (board[6] === player && board[7] === player && board[8] === player) {
+      return true;
+    }
+    if (board[0] === player && board[3] === player && board[6] === player) {
+      return true;
+    }
+    if (board[1] === player && board[4] === player && board[7] === player) {
+      return true;
+    }
+    if (board[2] === player && board[5] === player && board[8] === player) {
+      return true;
+    }
+    if (board[0] === player && board[4] === player && board[8] === player) {
+      return true;
+    }
+    if (board[2] === player && board[4] === player && board[6] === player) {
+      return true;
+    }
+  }
+
+  function tictactoe(board) {
+    if (isWinner(board, 'X')) {
+      console.log("XXXXXX wins! YAY!");
+      return 'X';
+    } else if (isWinner(board, 'O')) {
+      console.log("OOOOOO wins! YAY!");
+      return 'O';
+    } else {
+      return null;
+    }
+    return null;
+  }
 });
