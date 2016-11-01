5f86851ba04d7003ae36f795607b22abb9eb08e9
Juan A Cortes <juan.cortes@gmail.com>
2016-11-01 11:04:32 -0400

    Toggle Buttons: Make it so that when you click on a square, the first time, it changes to "o", the second time it changes to "x", and the third time it changes back to blank - "".

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
+  var times = 0;
   $(".square").on("click", function () {
-    $(this).text("O");
+    // debugger
+    if (times === 0) {
+      $(this).text("O");
+      times = 1;
+      console.log("Times var = " + times);
+    } else if (times === 1) {
+      $(this).text("X");
+      times = 2;
+      console.log("Times var in else if = " + times);
+    } else {
+      $(this).text("");
+      times = 0;
+      console.log(("Times var in else = " + times));
+    }
   });
 });
